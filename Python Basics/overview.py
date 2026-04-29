884ed5c7cb07c769244e228e662f7883e2088e5b
gufran30 <gufran3004@gmail.com>
2026-04-29 14:23:37 +0530

    Python overview

---
+++ Python Basics/overview.py
@@ -0,0 +1,38 @@
+# Python :
+
+#   - general-purpose interpreted, object-oriented, and high-level programming language
+#   - dynamically-typed programming language.
+#   - general purpose programming language known for its readability.
+
+#   Python is Interpreted − Python is processed at runtime by the interpreter. You do not need to compile your program before executing it. This is similar to PERL and PHP.
+
+# Python is Object-Oriented − Python supports Object-Oriented style or technique of programming that encapsulates code within objects.
+
+
+# Characteristics of Python :
+
+#   - supports automatic garbage collection.
+#   - supports functional and structured programming methods as well as OOP.
+#   - can be used as a scripting language
+#   - can be compiled to byte-code for building large applications.
+#   - can be easily integrated with other languages like C, C++, Java, etc.
+
+
+
+# Applications of Python :
+
+#   - Data Science : Python libraries like Numpy, Pandas, and Matplotlib are used for data analysis and visualization.
+#   - Web Development : Python frameworks like  Django, Pyramid, Flask make the development and deployment of Web Applications easy.
+#   - Computer Vision and Image processing
+#   - It is also favored in many tasks like Automation, Job Scheduling, GUI development, etc.
+
+
+
+# Pythonic Code Style :
+
+# Pythonic Code Style is actually more of a design philosophy and suggests to write a code which is :
+#   - Clean
+#   - Simple
+#   - Beautiful
+#   - Explicit
+#   - Readable
